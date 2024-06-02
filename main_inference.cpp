e69929a6c5417ffb44961d5bd08e38de9e30328e
kslav <kslav@utexas.edu>
2024-06-02 17:01:11 -0500

    add cpp inference script

---
+++ main_inference.cpp
@@ -0,0 +1,16 @@
+//Set up the environment
+
+#include <iostream>
+#include <torch/script.h>
+#include <memory>
+//#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+//Purpose: To load a trained model, load input images, and run inference to obtain a prediction 
+//Status: under development, configruing libtorch library and resolving compiler issues
+int main()
+{
+	cout << "Testing that the environment loads";
+	return 0;
+}
