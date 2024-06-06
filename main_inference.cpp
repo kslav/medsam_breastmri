9ab0f1665bfc094cf07563163f3b52711156cf1a
kslav <kslav@utexas.edu>
2024-06-05 22:26:31 -0400

    Updated cpp inference code

--- main_inference.cpp
+++ main_inference.cpp
@@ -11,6 +11,11 @@ using namespace std;
 //Status: under development, configruing libtorch library and resolving compiler issues
 int main()
 {
-	cout << "Testing that the environment loads";
-	return 0;
+	// First, test that the environment is built correctly by simply printing a torch tensor. 
+	// Comment this section out once you've verified it works!
+	cout << "Testing that the environment loads\n";
+  	torch::Tensor tensor = torch::rand({2, 3});
+  	cout << tensor << endl;
+
+  	// New-ish to C++, working on writing the rest
 }
