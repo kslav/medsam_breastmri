f24fe5acc9e5f72880b1a2c9fb2d85e9ad8cae4d
kslav <kslav@utexas.edu>
2024-06-12 22:00:43 -0400

    Add functionality in main for making customized figure for logging in WandB

--- main.py
+++ main.py
@@ -29,6 +29,9 @@ import random
 from datetime import datetime
 import shutil
 import glob
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
 
 
 # torch.distributed.init_process_group(backend="gloo")
@@ -152,19 +155,28 @@ def main_train(args):
                 # pick a random image in the batch
                 item_idx = np.random.randint(0,args.batch_size) 
                 # get the image, gt mask, and pred mask at item_idx and store in an array for easy access
-                img_array = [img_gt[item_idx,...], mask_gt[item_idx,...], medsam_pred[item_idx,...], boxes[item_idx,...]]
+                img = img_gt[item_idx,...] 
+                mask_arr = [mask_gt[item_idx,...], medsam_pred[item_idx,...]]
+                box = np.squeeze(boxes_np[item_idx,...])
                 # get everything off the GPU to CPU and convert to numpy
-                mask_np = img_array[1].detach().cpu().numpy()
-                img_np = img_array[0].permute(1, 2, 0).detach().cpu().numpy()
-                pred_np = img_array[2].detach().cpu().numpy()
+                img_np = img.permute(1, 2, 0).detach().cpu().numpy()
+                mask_np = np.squeeze(mask_arr[0].detach().cpu().numpy())
+                pred_np = np.squeeze(mask_arr[1].detach().cpu().numpy())
 
-                # Now make dictionaries to store the mask data in the format required by WandB
-                mask_dict_pred = {"mask_data": np.squeeze(pred_np),"class_labels": {0: "background", 1: "object"}}
-                mask_dict_gt = {"mask_data": np.squeeze(mask_np),"class_labels": {0: "background", 1: "object"}}
-                # Log the image with mask overlays using wandb.log
-                wandb.log({"ground_truth_mask": wandb.Image(img_np, masks={"ground truth": mask_dict_gt})})
-                wandb.log({"pred_mask": wandb.Image(img_np, masks={"prediction": mask_dict_pred})})
 
+                # # Now make dictionaries to store the mask data in the format required by WandB
+                # mask_dict_pred = {"mask_data": pred_np,"class_labels": {0: "background", 1: "object"}}
+                # mask_dict_gt = {"mask_data": mask_np,"class_labels": {0: "background", 1: "object"}}
+                # # Log the image with mask overlays using wandb.log
+                # wandb.log({"ground_truth_mask": wandb.Image(img_np, masks={"ground truth": mask_dict_gt})})
+                # wandb.log({"pred_mask": wandb.Image(img_np, masks={"prediction": mask_dict_pred})})
+
+                # Testing out our make_fig_for_logging(img,mask_arr) function
+                mask_arr_forFig = [mask_np[...,None],pred_np[...,None]]
+                fig = make_image_for_logging(img_np,mask_arr_forFig,box)
+
+                # Log the figure we made
+                wandb.log({"Comparison": wandb.Image(fig)})
                 # Log the step-level loss here:
                 wandb.log({"loss_stepwise": loss.item()})
 
@@ -198,6 +210,52 @@ def main_train(args):
 
             torch.save(checkpoint, join(model_save_path, "medsam_model_best.pth"))
 
+### The following three functions are for visualization purposes in WandB
+
+def show_mask(mask, ax, random_color=False):
+    # Borrowed from MedSAM_Inference.py by the original authors of MedSAM, Ma et al.
+    # mask is 2D
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([251 / 255, 252 / 255, 30 / 255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def show_box(box, ax):
+    # Borrowed from MedSAM_Inference.py by the original authors of MedSAM, Ma et al.
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor="blue", facecolor=(0, 0, 0, 0), lw=2))
+
+def make_image_for_logging(img, mask_arr,box):
+    # PURPOSE: To design a figure where we have the image with ground truth mask overlayed on the left
+    # and the prediction mask overlayed on the right, side by side. Once we've made this image, we store
+    # it in the buffer, retrieve it from the buffer, and then log it with WandB.
+    # img: imag of size 1024x1024x3, randomly selected from current batch
+    # mask_arr: array of size 2, containing ground truth [0] and prediction masks [1]
+
+    # make the figure with matplotlib
+    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+    ax[0].imshow(img)
+    show_mask(mask_arr[0], ax[0])
+    ax[0].set_title("Input Image and Ground Truth")
+    ax[1].imshow(img_3c)
+    show_mask(mask_arr[1], ax[1])
+    show_box(box, ax[1])
+    ax[1].set_title("MedSAM Segmentation")
+    plt.tight_layout()
+
+    buf = BytesIO()  # create a BytesIO buffer
+    fig.savefig(buf, format='png')  # save the plot to the buffer in PNG format
+    buf.seek(0)  # rewind the buffer to the beginning
+    img_out = Image.open(buf)  # open the image from the buffer
+
+    plt.close(fig)  # close the figure to free memory
+    return img_out
+
 
 if __name__ == "__main__":
     
@@ -230,7 +288,7 @@ if __name__ == "__main__":
 
     # Logging parameters
     parser.add_argument("--use_wandb", action='store', dest='use_wandb', type=bool,default=False, help="use wandb to monitor training")
-    parser.add_argument("--log_frequency", action='store',dest='log_frequency',type=int, default=500, help="define N for logging every N steps to save on memory")
+    parser.add_argument("--log_frequency", action='store',dest='log_frequency',type=int, default=100, help="define N for logging every N steps to save on memory")
 
     args = parser.parse_args()
 
