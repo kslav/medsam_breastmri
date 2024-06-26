c1bee76854674488fe7d4deceae42f3643a8367b
kslav <kslav@utexas.edu>
2024-06-26 14:24:05 -0400

    refixing import error

--- medsam_inference.py
+++ medsam_inference.py
@@ -115,7 +115,7 @@ medsam_model = medsam_model.to(device)
 medsam_model.eval()
 
 ### Create Dataloader ###
-dataSet = NpyDataset(args.data_path, transform=transform.ToTensor())
+dataSet = NpyDataset(args.data_path, transform=transforms.ToTensor())
 dataLoader = DataLoader(dataSet,batch_size=1,shuffle=False,num_workers=2,pin_memory=False)
 
 ### Loop through images and masks, creating a gt bounding box and running inference ####
